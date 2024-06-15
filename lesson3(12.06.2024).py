0c25752acbe3a7370dda77ade31f8b02eeeba83d
Bakhtiyar Tukhtasinov <144336544+tuxtasinow@users.noreply.github.com>
2024-06-15 22:58:41 +0500

    Add files via upload

---
+++ lesson3(12.06.2024).py
@@ -0,0 +1,11 @@
+name = 'Bakhtiyar'  #Строка
+print(name, type(name))
+
+age = 25  #Целое число
+print(age, type(age))
+
+age = age + 15.5  #Число с плавающей точкой
+print(age, type(age))
+
+is_student = True  #Булевой тип
+print(is_student, type(is_student))
